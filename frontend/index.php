6ca97559df0481e8774e523ce261ecc3e660b251
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 17:36:13 +0000

    Formatting
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- frontend/index.php
+++ frontend/index.php
@@ -1,22 +1,23 @@
 <?php
-	/*
-	**	Guestbook frontend
-	*/
-	
-	global $current_user;
-	global $wpdb;
-	
-	// Load settings, if not set
-	global $gwolle_gb_settings;
-	if (!isset($gwolle_gb_settings)) {
-    include_once(GWOLLE_GB_DIR.'/functions/gwolle_gb_get_settings.func.php');
-    gwolle_gb_get_settings();
-  }
-    
-  if ($_REQUEST['gb_page'] == 'write') {  //	Write mode
-		include('write.php');
-	}
-	else { //	Read mode
-		include('read.php');
-	}
+/*
+ **	Guestbook frontend
+ */
+
+global $current_user;
+global $wpdb;
+
+// Load settings, if not set
+global $gwolle_gb_settings;
+if (!isset($gwolle_gb_settings)) {
+	include_once (GWOLLE_GB_DIR . '/functions/gwolle_gb_get_settings.func.php');
+	gwolle_gb_get_settings();
+}
+
+if ($_REQUEST['gb_page'] == 'write') {//	Write mode
+	include ('write.php');
+} else {//	Read mode
+	include ('read.php');
+}
+
+
 ?>
