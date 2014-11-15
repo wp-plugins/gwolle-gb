8a7832dad44507d5ec52e171abc43763149f5f53
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 18:41:06 +0000

    Many changes and cleanups
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026346 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/gwolle_gb_get_settings.func.php
+++ functions/gwolle_gb_get_settings.func.php
@@ -1,62 +1,44 @@
 <?php
-  if (!function_exists('gwolle_gb_get_settings')) {
-    /**
-     * gwolle_gb_get_settings
-     * Gets the Gwolle-GB settings from database
-     * and stores them in a global variable.
-     * Return:
-     * - TRUE, if settings were loaded successfully
-     * - FALSE, if loading settings failed
-     */
-    function gwolle_gb_get_settings($args=array()) {
-      global $gwolle_gb_settings;
-      global $wpdb;
-      
-      //  Declare database table names
-      $wpdb->gwolle_gb_entries  = $wpdb->prefix.'gwolle_gb_entries';
-      $wpdb->gwolle_gb_log      = $wpdb->prefix.'gwolle_gb_log';
-      
-      $sql = "
-      SELECT
-        REPLACE(o.option_name, 'gwolle_gb-', '') AS name,
-        o.option_value AS value
-      FROM
-        ".$wpdb->options." o
-      WHERE
-        o.option_name LIKE 'gwolle_gb-%'
-      ORDER BY
-        o.option_name";
-      $options = $wpdb->get_results($sql, ARRAY_A);
-      if ($wpdb->num_rows == 0) {
-        return FALSE;
-      } else {
 
-		foreach ($options as $option) {
-          if (in_array($option['value'], array('true', 'false'))) {
-            $value = ($option['value'] == 'true') ? TRUE : FALSE;
-          }
-          else {
-            $value = $option['value'];
-          }
-          $gwolle_gb_settings[$option['name']] = $value;
-        }
-        
-        /**
-         *  Now add some hard coded settings.
-         */
-        //  Entries per page (backend)
-        $gwolle_gb_settings['entries_per_page'] = 15;
-        //  Default mail text
-        $gwolle_gb_settings['defaultMailText'] = __("Hello,\n\nthere is a new guestbook entry at '%blog_name%'.\nYou can check it at %entry_management_url%.\n\nHave a nice day!\nYour Gwolle-GB-Mailer",GWOLLE_GB_TEXTDOMAIN);
-        if (defined('GWOLLE_GB_ACCESS_LEVEL') === FALSE) {
-          if (get_option('gwolle_gb-access-level') === FALSE) {
-            define('GWOLLE_GB_ACCESS_LEVEL', 10);
-          //} else {
-            define('GWOLLE_GB_ACCESS_LEVEL', $gwolle_gb_settings['access-level']);
-          }
-        }
-        return TRUE;
-      }
-    }
+if (!function_exists('gwolle_gb_get_settings')) {
+	/*
+	 * gwolle_gb_get_settings
+	 * Gets the Gwolle-GB settings from database and stores them in a global variable.
+	 *
+	 * $args is not being used anymore
+	 * Return: true (always)
+	 */
+
+	function gwolle_gb_get_settings($args = array()) {
+		global $gwolle_gb_settings;
+		global $wpdb;
+
+
+		// FIXME, set useful defaults, also in settingspage
+		// FIXME, let all code use just the WP options through get_options.
+		// FIXME, set these options on activation in the install function
+		// Just the list of options
+		$default = "";
+		$gwolle_gb_settings['adminMailContent']	= get_option( 'gwolle_gb-adminMailContent', $default );
+		$default = __("Hello,\n\nthere is a new guestbook entry at '%blog_name%'.\nYou can check it at %entry_management_url%.\n\nHave a nice day!\nYour Gwolle-GB-Mailer", GWOLLE_GB_TEXTDOMAIN);
+		$gwolle_gb_settings['defaultMailText']	= get_option( 'gwolle_gb-defaultMailText', $default );
+		$gwolle_gb_settings['akismet-active']	= get_option( 'gwolle_gb-akismet-active', false );
+		$gwolle_gb_settings['checkForImport']	= get_option( 'gwolle_gb-checkForImport' );		// false
+		$gwolle_gb_settings['entriesPerPage']	= get_option( 'gwolle_gb-entriesPerPage', 15 );	// Frontend option
+		$gwolle_gb_settings['guestbookOnly']	= get_option( 'gwolle_gb-guestbookOnly' );		// false
+		$gwolle_gb_settings['linkAuthorWebsite']= get_option( 'gwolle_gb-linkAuthorWebsite' );	// false
+		$gwolle_gb_settings['moderate-entries']	= get_option( 'gwolle_gb-moderate-entries' );	// false
+		$gwolle_gb_settings['notifyByMail-1']	= get_option( 'gwolle_gb-notifyByMail-1' );		// true
+		$gwolle_gb_settings['post_ID']			= get_option( 'gwolle_gb-post_ID' );			// 0
+		$gwolle_gb_settings['recaptcha-active']	= get_option( 'gwolle_gb-recaptcha-active' );	// false
+		$gwolle_gb_settings['showLineBreaks']	= get_option( 'gwolle_gb-showLineBreaks' );		// false
+		$gwolle_gb_settings['showSmilies']		= get_option( 'gwolle_gb-showSmilies', false );
+		$gwolle_gb_settings['checkForImport']	= get_option( 'gwolle_gb-checkForImport', false );
+		$gwolle_gb_settings['showEntryIcons']	= get_option( 'gwolle_gb-showEntryIcons', false );
+		$gwolle_gb_settings['entries_per_page']	= get_option( 'gwolle_gb-entries_per_page', 15 );
+
+		return TRUE;
 	}
+
+}
 ?>
