601269d99116af60adb56699555704ab96623807
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 18:53:04 +0000

    Formatting
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026366 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/check_entry.func.php
+++ admin/check_entry.func.php
@@ -1,19 +1,19 @@
 <?php
-	/*
-	**	Function to check/uncheck an entry.
-	*/
+/*
+ **	Function to check/uncheck an entry.
+ */
 
-	function check_gwolle_gb_entry($entry_id, $newCheckedStatus='checked') {
-		global $wpdb;
-		global $current_user;
+function check_gwolle_gb_entry($entry_id, $newCheckedStatus = 'checked') {
+	global $wpdb;
+	global $current_user;
 
-		//	Let's first check if there's anything to do. Maybe the entry already has the $newCheckedStatus.
-		if ($newCheckedStatus == 'checked') {
-			$isChecked = '1';
-		} else {
-			$isChecked = '0';
-		}
-		$currentStatus_result = $wpdb->query("
+	//	Let's first check if there's anything to do. Maybe the entry already has the $newCheckedStatus.
+	if ($newCheckedStatus == 'checked') {
+		$isChecked = '1';
+	} else {
+		$isChecked = '0';
+	}
+	$currentStatus_result = $wpdb->query("
 			SELECT *
 			FROM
 				" . $wpdb->prefix . "gwolle_gb_entries e
@@ -22,20 +22,20 @@
 				AND
 				e.entry_isChecked = '" . $isChecked . "'
 		");
-		if ($currentStatus_result == 1) {
-			//	The entry already has the status we want to apply. Do not proceed here.
-			return false;
+	if ($currentStatus_result == 1) {
+		//	The entry already has the status we want to apply. Do not proceed here.
+		return false;
+	} else {
+		//	Let's apply the new status.
+		if ($newCheckedStatus == 'checked') {
+			$checkedBy = $current_user -> data -> ID;
+			$log_subject = 'entry-checked';
 		} else {
-			//	Let's apply the new status.
-			if ($newCheckedStatus == 'checked') {
-				$checkedBy = $current_user->data->ID;
-				$log_subject = 'entry-checked';
-			} else {
-				$checkedBy = '0';
-				$log_subject = 'entry-unchecked';
-			}
+			$checkedBy = '0';
+			$log_subject = 'entry-unchecked';
+		}
 
-			$entryCheck_result = $wpdb->query("
+		$entryCheck_result = $wpdb->query("
 				UPDATE
 					" . $wpdb->prefix . "gwolle_gb_entries
 				SET
@@ -46,14 +46,10 @@
 				LIMIT 1
 			");
 
-			if ($entryCheck_result === 1) {
-				//	Write a log entry on this.
-				include_once(WP_PLUGIN_DIR.'/gwolle-gb/functions/gwolle_gb_add_log_entry.func.php');
-				return gwolle_gb_add_log_entry(array(
-					'subject'     => $log_subject,
-					'subject_id'  => $entry_id
-				));
-			}
+		if ($entryCheck_result === 1) {
+			//	Write a log entry on this.
+			return gwolle_gb_add_log_entry(array('subject' => $log_subject, 'subject_id' => $entry_id));
 		}
 	}
+}
 ?>
