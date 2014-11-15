6ad470a78a2aa6f1a68349541a158b04b650004b
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 18:48:10 +0000

    Reorganized
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026359 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- frontend/index.php
+++ frontend/index.php
@@ -1,23 +1,39 @@
 <?php
 /*
- **	Guestbook frontend
+ * Guestbook frontend
+ * Called by using the shortcode [gwolle_gb] in a page or post.
+ * $output will be used as replacement for that shortcode using the Shortcode API.
  */
 
-global $current_user;
-global $wpdb;
 
-// Load settings, if not set
-global $gwolle_gb_settings;
-if (!isset($gwolle_gb_settings)) {
-	include_once (GWOLLE_GB_DIR . '/functions/gwolle_gb_get_settings.func.php');
-	gwolle_gb_get_settings();
-}
+/* Frontend Function
+ * Use this to display the guestbook on a page without using a shortcode
+ */
 
-if ($_REQUEST['gb_page'] == 'write') {//	Write mode
-	include ('write.php');
-} else {//	Read mode
-	include ('read.php');
+function show_gwolle_gb() {
+	echo get_gwolle_gb();
 }
 
 
-?>
+function get_gwolle_gb() {
+
+	// Load Frontend CSS in Footer, only when it's active
+	wp_enqueue_style('gwolle_gb_frontend_css', plugins_url('style.css', __FILE__), 'screen');
+
+	// Define $output
+	$output = '<div id="gwolle_gb">';
+
+	// Add the form
+	$output .= gwolle_gb_frontend_write();
+
+	// Add the list of entries to show
+	$output .= gwolle_gb_frontend_read();
+
+	$output .= '</div>';
+
+	return $output;
+}
+
+add_shortcode( 'gwolle-gb', 'get_gwolle_gb' ); // deprecated, do not use dashes in Shortcode API
+add_shortcode( 'gwolle_gb', 'get_gwolle_gb' );
+
