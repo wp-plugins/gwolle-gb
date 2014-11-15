4506bd97573ec0ca4faa01771871888f47f694a9
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 18:57:27 +0000

    Remove includes
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026373 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/do-spam.php
+++ admin/do-spam.php
@@ -3,10 +3,8 @@
 	**	Handles spam/no spam-requests for guestbook entries
 	*/
 
-	//	No direct calls to this script
+	// No direct calls to this script
 	if (preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('No direct calls allowed!'); }
-
-	include('spam.func.php');
 
 	if ($_REQUEST['action'] == 'markSpam') {
 		$markAs = 'spam';
@@ -15,4 +13,4 @@
 	}
 
 	spam_gwolle_gb_entry($_REQUEST['entry_id'],$markAs,true,$_REQUEST['show']);
-?>
+
