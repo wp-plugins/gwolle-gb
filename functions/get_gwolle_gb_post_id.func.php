d5f6b77ca7dde3fa1df7696faed72541a93a457c
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 18:32:05 +0000

    Formatting
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026329 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/get_gwolle_gb_post_id.func.php
+++ functions/get_gwolle_gb_post_id.func.php
@@ -1,32 +1,31 @@
 <?php
-  /**
-   * get_gwolle_gb_post_id
-   * Searches for the post with the gwolle-gb tag.
-   * Returns:
-   * - FALSE, if search was not successful
-   * - (int)$post_id, if found
-   */
-  if (!function_exists('get_gwolle_gb_post_id')) {
-    function get_gwolle_gb_post_id() {
-      global $wpdb;
-      $sql = "
-	    SELECT
-	      p.ID
-	    FROM
-	      ".$wpdb->posts." p
-	    WHERE
-	      p.post_content LIKE '%[gwolle-gb]%'
-	      AND
-	      p.post_status = 'publish'
-	    LIMIT 1";
-	  $result = $wpdb->query($sql);
-	  if ($wpdb->num_rows == 0) {
-        return 0;
-      } else {
-        $data = $wpdb->get_results($sql, ARRAY_A);
-        return $data[0]['ID'];
-      }
-      return FALSE;
-    }
-  }
-?>
+/**
+ * get_gwolle_gb_post_id
+ * Searches for the post with the gwolle-gb tag.
+ * Returns:
+ * - FALSE, if search was not successful
+ * - (int) $post_id, if found
+ */
+
+if (!function_exists('get_gwolle_gb_post_id')) {
+	function get_gwolle_gb_post_id() {
+		global $wpdb;
+		$sql = "
+		SELECT
+			p.ID
+		FROM
+			" . $wpdb -> posts . " p
+		WHERE
+			p.post_content LIKE '%[gwolle-gb]%'
+			AND
+			p.post_status = 'publish'
+		LIMIT 1";
+		$data = $wpdb->get_results($sql, ARRAY_A);
+		if (count($data) == 0) {
+			return 0;
+		} else {
+			return $data[0]['ID'];
+		}
+		return FALSE;
+	}
+}
