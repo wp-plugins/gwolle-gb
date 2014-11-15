5abd186993b8460ea4b51c5b137b4a23a0309e67
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 18:43:41 +0000

    Formatting
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026349 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/gwolle_gb_mark_spam.func.php
+++ functions/gwolle_gb_mark_spam.func.php
@@ -8,17 +8,15 @@ if (!function_exists('gwolle_gb_mark_spam')) {
 		global $wpdb;
 		global $current_user;
 
-		//  Check permission
-		if (!current_user_can('level_' . GWOLLE_GB_ACCESS_LEVEL)) {
-			//	The current user's not allowed to do this.
-			header('Location: ' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=' . GWOLLE_GB_FOLDER . '/gwolle-gb.php&msg=no-permission');
-			exit ;
+		// Check permission
+		if (!current_user_can('moderate_comments')) {
+			// The current user's not allowed to do this.
+			die(__('Cheatin&#8217; uh?'));
 		}
 
 		// Load settings, if not set
 		global $gwolle_gb_settings;
 		if (!isset($gwolle_gb_settings)) {
-			include_once (GWOLLE_GB_DIR . '/functions/gwolle_gb_get_settings.func.php');
 			gwolle_gb_get_settings();
 		}
 
@@ -37,73 +35,65 @@ if (!function_exists('gwolle_gb_mark_spam')) {
 			exit ;
 		}
 
-		if (!isset($args['entry_id']) || (int)$args['entry_id'] === 0) {
+		if (!isset($args['entry_id']) || (int) $args['entry_id'] === 0) {
 			return FALSE;
 		}
 		//  Check if the entry exists
-		include_once (GWOLLE_GB_DIR . '/functions/gwolle_gb_get_entries.func.php');
-		$entry = gwolle_gb_get_entries(array('entry_id' => $args['entry_id']));
+		$entry = gwolle_gb_get_entries_old(array('entry_id' => $args['entry_id']));
 		if ($entry === FALSE) {
 			return FALSE;
 		}
 
-		//  Generate the Akismet object
-		if (version_compare(phpversion(), '5.0', '>=')) {//	Use the PHP5 class
-			if (!class_exists('Akismet')) {
-				//	Only include if the Akismet class doesn't already exist.
-				include (AKISMET_PHP5_CLASS_DIR . '/Akismet.class.php');
+		/**
+		 * Check for spam using Akismet, if this has been set in the
+		 * settings dialog of Gwolle-GB and if there's a WordPress API key defined.
+		 */
+		if (get_option('gwolle_gb-akismet-active')) {
+			if (class_exists('Akismet')) {
+				$args['is_spam'] = 0;
+				$wordpressApiKey = get_option('wordpress_api_key');
+				if ($gwolle_gb_settings['akismet-active'] === TRUE && strlen($wordpressApiKey) > 0 && !isset($args['bypass_akismet'])) {
+					$isspam = gwolle_gb_isspam_akismet_old( $args );
+					if ($isspam === true) {
+						//	Akismet detected spam.
+						$args['is_spam'] = 1;
+					}
+				}
 			}
-
-			$akismet = new Akismet(get_bloginfo('wpurl'), $wordpress_api_key);
-			$akismet -> setCommentAuthor($entry['entry_author_name']);
-			$akismet -> setCommentAuthorEmail($entry['entry_author_email']);
-			$akismet -> setCommentAuthorURL($entry['entry_author_website']);
-			$akismet -> setCommentContent($entry['entry_content']);
-			$akismet -> setPermalink(get_bloginfo('wpurl'));
-		} elseif (version_compare(phpversion(), '4.0', '>=')) {//	Use the PHP4 class
-			if (!class_exists('Akismet')) {
-				//	Only include if the Akismet class doesn't already exist.
-				include (AKISMET_PHP4_CLASS_DIR . '/Akismet.class.php');
-			}
-			$comment = array('author' => $entry['entry_author_name'], 'email' => $entry['entry_author_email'], 'website' => $entry['entry_author_website'], 'body' => $entry['entry_content'], 'permalink' => get_bloginfo('wpurl'));
-			$akismet = new Akismet(get_bloginfo('wpurl'), $wordpress_api_key, $comment);
 		}
 
-		// Include the function to generate a log entry
-		include_once (GWOLLE_GB_DIR . '/functions/gwolle_gb_add_log_entry.func.php');
-
 		// What shall we do with the entry?
-		if (isset($args['no_spam']) && $args['no_spam'] === TRUE) {
-			//  This is no spam.
-			if ($akismet) {
-				$akismet -> submitHam();
+		if (isset($args['no_spam']) && $args['no_spam'] === true) {
+			//  User says it is no spam, so inform akismet of the false positive.
+			if ($args['is_spam'] == 1) {
+				// $akismet->submitHam(); // FIXME
 			}
 			$sql = "
-        UPDATE
-          " . $wpdb -> gwolle_gb_entries . "
-        SET
-          entry_isSpam = 0
-        WHERE
-          entry_id = " . (int)$args['entry_id'] . "
-        LIMIT 1";
+				UPDATE
+					" . $wpdb->gwolle_gb_entries . "
+				SET
+					entry_isSpam = 0
+				WHERE
+					entry_id = " . (int) $args['entry_id'] . "
+				LIMIT 1";
 			$result = $wpdb->query($sql);
 			if ($result == 1) {
 				gwolle_gb_add_log_entry(array('subject' => 'marked-as-not-spam', 'subject_id' => $args['entry_id']));
 				return TRUE;
 			}
 		} else {
-			//  This is spam
-			if ($akismet) {
-				$akismet -> submitSpam();
+			//  User says it is spam, so inform akismet of the false negative.
+			if ($args['is_spam'] == 0) {
+				//$akismet -> submitSpam(); // FIXME
 			}
 			$sql = "
-        UPDATE
-          " . $wpdb -> gwolle_gb_entries . "
-        SET
-          entry_isSpam = 1
-        WHERE
-          entry_id = " . (int)$args['entry_id'] . "
-        LIMIT 1";
+				UPDATE
+					" . $wpdb->gwolle_gb_entries . "
+				SET
+					entry_isSpam = 1
+				WHERE
+					entry_id = " . (int) $args['entry_id'] . "
+				LIMIT 1";
 			$result = $wpdb->query($sql);
 			if ($result == 1) {
 				gwolle_gb_add_log_entry(array('subject' => 'marked-as-spam', 'subject_id' => $args['entry_id']));
