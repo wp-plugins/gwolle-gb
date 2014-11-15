55ba100b36b472466bae6479bb13069924ed65d1
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 18:47:37 +0000

    Formatting and many changes and fixes
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026358 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- frontend/do-saveNewEntry.php
+++ frontend/do-saveNewEntry.php
@@ -1,44 +1,37 @@
 <?php
 	/*
-	**	Save new entries to the database, when valid.
-	**	Obligatory fields:
-	**	- name
-	**	- entry
-	**	... and a negative Akismet result (= no spam) and a correct captcha; both only when turned on in the settings panel.
-	*/
-	
+	 * Save new entries to the database, when valid.
+	 * Obligatory fields:
+	 * - name
+	 * - entry
+	 * ... and a negative Akismet result (= no spam) and a correct captcha; both only when turned on in the settings panel.
+	 */
+
 	//	No direct calls to this script
 	if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('No direct calls allowed!'); }
-	
+
 	global $wpdb;
-	
+
 	// Load settings, if not set
 	global $gwolle_gb_settings;
 	if (!isset($gwolle_gb_settings)) {
-    include_once(GWOLLE_GB_DIR.'/functions/gwolle_gb_get_settings.func.php');
-    gwolle_gb_get_settings();
-  }
-  
-  include_once(GWOLLE_GB_DIR.'/functions/gwolle_gb_save_entry.func.php');
-  $entry_id = gwolle_gb_save_entry();
-  
-  // Get links to guestbook page
-	include_once(GWOLLE_GB_DIR.'/functions/gwolle_gb_get_link.func.php');
-	$gb_links = gwolle_gb_get_link(array(
-    'all' => TRUE
-  ));
-  
-  if ($entry_id === FALSE) {
-    header('Location: '.$gb_links['write']);
+		gwolle_gb_get_settings();
+	}
+
+	$entry_id = gwolle_gb_save_entry();
+
+	// Get links to guestbook page
+	$gb_links = gwolle_gb_get_link(array( 'all' => TRUE ));
+
+	if ($entry_id === FALSE) {
+		header('Location: '.$gb_links['write']);
+		exit;
+	} else {
+		$msg = __('Thanks for your entry.',GWOLLE_GB_TEXTDOMAIN);
+		if ($gwolle_gb_settings['moderate-entries'] === TRUE) {
+			$msg .= __('<br>We will review it and unlock it in a short while.',GWOLLE_GB_TEXTDOMAIN);
+		}
+		$_SESSION['gwolle_gb']['msg'] = $msg;
+		header('Location: '.$gb_links['read']);
 		exit;
 	}
-	else {
-    $msg = __('Thanks for your entry.',GWOLLE_GB_TEXTDOMAIN);
-    if ($gwolle_gb_settings['moderate-entries'] === TRUE) {
-      $msg .= __('<br>We will review it and unlock it in a short while.',GWOLLE_GB_TEXTDOMAIN);
-    }
-    $_SESSION['gwolle_gb']['msg'] = $msg;
-    header('Location: '.$gb_links['read']);
-    exit;
-  }
-?>
