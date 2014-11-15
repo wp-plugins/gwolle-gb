382ff9db20b4ee7a61cca30e10cb9a49aef608d5
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-15 17:26:58 +0000

    Use
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1026253 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/check_entry.func.php
+++ admin/check_entry.func.php
@@ -2,14 +2,18 @@
 	/*
 	**	Function to check/uncheck an entry.
 	*/
-	
+
 	function check_gwolle_gb_entry($entry_id, $newCheckedStatus='checked') {
 		global $wpdb;
 		global $current_user;
-		
+
 		//	Let's first check if there's anything to do. Maybe the entry already has the $newCheckedStatus.
-		if ($newCheckedStatus == 'checked') { $isChecked = '1'; } else { $isChecked = '0'; }
-		$currentStatus_result = mysql_query("
+		if ($newCheckedStatus == 'checked') {
+			$isChecked = '1';
+		} else {
+			$isChecked = '0';
+		}
+		$currentStatus_result = $wpdb->query("
 			SELECT *
 			FROM
 				" . $wpdb->prefix . "gwolle_gb_entries e
@@ -18,21 +22,19 @@
 				AND
 				e.entry_isChecked = '" . $isChecked . "'
 		");
-		if (mysql_num_rows($currentStatus_result) == 1) {
+		if ($currentStatus_result == 1) {
 			//	The entry already has the status we want to apply. Do not proceed here.
 			return false;
-		}
-		else {
+		} else {
 			//	Let's apply the new status.
 			if ($newCheckedStatus == 'checked') {
 				$checkedBy = $current_user->data->ID;
 				$log_subject = 'entry-checked';
-			}
-			else {
+			} else {
 				$checkedBy = '0';
 				$log_subject = 'entry-unchecked';
 			}
-			
+
 			$entryCheck_result = $wpdb->query("
 				UPDATE
 					" . $wpdb->prefix . "gwolle_gb_entries
@@ -43,13 +45,13 @@
 					entry_id = '" . $entry_id . "'
 				LIMIT 1
 			");
-			
-			if (mysql_affected_rows() === 1) {
+
+			if ($entryCheck_result === 1) {
 				//	Write a log entry on this.
 				include_once(WP_PLUGIN_DIR.'/gwolle-gb/functions/gwolle_gb_add_log_entry.func.php');
 				return gwolle_gb_add_log_entry(array(
-				  'subject'     => $log_subject,
-				  'subject_id'  => $entry_id
+					'subject'     => $log_subject,
+					'subject_id'  => $entry_id
 				));
 			}
 		}
