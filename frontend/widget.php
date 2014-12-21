781fe5d442320026b68b27f3f61d4fdc20826865
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-21 19:54:20 +0000

    Tag 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/tags/1.0.5@1051041 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ frontend/widget.php
@@ -0,0 +1,243 @@
+<?php
+/*
+ * widget.php
+ * Gwolle-GB widget
+ */
+
+if (function_exists('register_sidebar') && class_exists('WP_Widget')) {
+	class GwolleGB_Widget extends WP_Widget {
+
+		/* Constructor */
+		function __construct() {
+			$widget_ops = array( 'classname' => 'gwolle_gb', 'description' => __('Displays the recent guestbook entries.',GWOLLE_GB_TEXTDOMAIN) );
+			parent::__construct('gwolle_gb', 'Gwolle-GB Widget', $widget_ops);
+			$this->alt_option_name = 'gwolle_gb';
+		}
+
+		/** @see WP_Widget::widget */
+		function widget($args, $instance) {
+			extract($args);
+
+			$widget_title = (isset($instance['title']) && strlen(trim($instance['title'])) > 0) ? $instance['title'] : __('Guestbook', GWOLLE_GB_TEXTDOMAIN);
+
+			$num_entries = (isset($instance['num_entries']) && (int)$instance['num_entries'] > 0) ? (int)$instance['num_entries'] : 5;
+
+			$best = (isset($instance['best']) && strlen(trim($instance['best'])) > 0) ? $instance['best'] : '';
+			$best = explode(",", $best);
+
+			$num_words = (isset($instance['num_words']) && (int)$instance['num_words'] > 0) ? (int)$instance['num_words'] : 10;
+
+			$link_text = (isset($instance['link_text']) && strlen(trim($instance['link_text'])) > 0) ? $instance['link_text'] : __('Visit guestbook', GWOLLE_GB_TEXTDOMAIN);
+
+			$postid = (isset($instance['postid']) && (int)$instance['postid'] > 0) ? (int)$instance['postid'] : 0;
+
+			// Init
+			$widget_html = '';
+
+			$widget_html .= $before_widget;
+			$widget_html .= '<div class="gwolle_gb_widget">';
+			if ($widget_title !== FALSE) {
+				$widget_html .= $before_title . apply_filters('widget_title', $widget_title) . $after_title;
+			}
+
+			$widget_html .= '<ul class="gwolle_gb_widget">';
+			$counter = 0;
+
+			// Get the best entries first
+			if ( is_array( $best ) && count( $best ) > 0 ) {
+				foreach ($best as $entry_id) {
+					if ( $counter == $num_entries) { break; } // we have enough
+					$entry = new gwolle_gb_entry();
+					$entry_id = intval($entry_id);
+					if ( isset($entry_id) && $entry_id > 0 ) {
+						$result = $entry->load( $entry_id );
+						if ( !$result ) {
+							// No entry loaded
+							continue;
+						}
+						// Main Content
+						$widget_html .= '
+										<li class="gwolle_gb_widget">
+										';
+						$entry_content = gwolle_gb_get_excerpt( $entry->get_content(), $num_words );
+						if ( get_option('gwolle_gb-showSmilies', 'true') === 'true' ) {
+							$entry_content = convert_smilies($entry_content);
+						}
+						$widget_html .= $entry_content;
+						$widget_html .= '
+										</li>
+										';
+						$counter++;
+					}
+				}
+			}
+
+			// Get the latest $num_entries guestbook entries
+			if ( $counter != $num_entries) { // we have enough
+				$entries = gwolle_gb_get_entries(
+					array(
+						'num_entries' => $num_entries,
+						'checked' => 'checked',
+						'deleted' => 'notdeleted',
+						'spam' => 'nospam'
+						)
+					);
+				if ( is_array( $entries ) && count( $entries ) > 0 ) {
+					foreach( $entries as $entry ) {
+						if ( $counter == $num_entries) { break; } // we have enough
+						if ( is_array( $best) && in_array( $entry->get_id(), $best ) ) { continue; } // already listed
+						// Main Content
+						$widget_html .= '
+										<li class="gwolle_gb_widget">
+										';
+						$entry_content = gwolle_gb_get_excerpt( $entry->get_content(), $num_words );
+						if ( get_option('gwolle_gb-showSmilies', 'true') === 'true' ) {
+							$entry_content = convert_smilies($entry_content);
+						}
+						$widget_html .= $entry_content;
+						$widget_html .= '
+										</li>
+										';
+						$counter++;
+					}
+				}
+			}
+
+			$widget_html .= '</ul>';
+
+			// Post the link to the Guestbook.
+			if ( (int) $postid > 0 ) {
+				$widget_html .= '
+				<p class="gwolle_gb_link">
+					<a href="' . get_site_url() . "/?p=" . $postid . '" title="' . __('Click here to get to the guestbook.', GWOLLE_GB_TEXTDOMAIN) . '">' . $link_text . ' &raquo;</a>
+				</p>';
+			}
+			$widget_html .= '</div>' . $after_widget;
+
+			if ( $counter > 0 ) {
+				// Only display widget if there are any entries
+				echo $widget_html;
+			}
+		}
+
+		/** @see WP_Widget::update */
+		function update($new_instance, $old_instance) {
+			$instance = $old_instance;
+			$instance['title']       = strip_tags($new_instance['title']);
+			$instance['num_entries'] = (int) strip_tags($new_instance['num_entries']);
+			$instance['best']        = strip_tags($new_instance['best']);
+			$instance['num_words']   = (int) strip_tags($new_instance['num_words']);
+			$instance['link_text']   = strip_tags($new_instance['link_text']);
+			$instance['postid']      = (int) strip_tags($new_instance['postid']);
+
+			return $instance;
+		}
+
+		/** @see WP_Widget::form */
+		function form($instance) {
+
+			$default_value = array(
+						"title" => __('Guestbook', GWOLLE_GB_TEXTDOMAIN),
+						"num_entries" => 5,
+						"best" => '',
+						"num_words" => 10,
+						"link_text" => __('Visit guestbook', GWOLLE_GB_TEXTDOMAIN),
+						"postid" => 0
+				);
+			$instance      = wp_parse_args( (array) $instance, $default_value );
+
+			$title         = esc_attr($instance['title']);
+			$num_entries   = (int) esc_attr($instance['num_entries']);
+			$best          = esc_attr($instance['best']);
+			$num_words     = (int) esc_attr($instance['num_words']);
+			$link_text     = esc_attr($instance['link_text']);
+			$postid        = (int) esc_attr($instance['postid']);
+			?>
+
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>" /><?php _e('Title:', GWOLLE_GB_TEXTDOMAIN); ?></label>
+				<br />
+				<input type="text" id="<?php echo $this->get_field_id('title'); ?>" value="<?php echo $title; ?>" name="<?php echo $this->get_field_name('title'); ?>" />
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id('num_entries'); ?>" /><?php _e('Number of entries:', GWOLLE_GB_TEXTDOMAIN); ?></label>
+				<br />
+				<select id="<?php echo $this->get_field_id('num_entries'); ?>" name="<?php echo $this->get_field_name('num_entries'); ?>">
+					<?php
+					for ($i = 1; $i <= 15; $i++) {
+						echo '<option value="' . $i . '"';
+						if ( $i === $num_entries ) {
+							echo ' selected="selected"';
+						}
+						echo '>' . $i . '</option>';
+					}
+					?>
+				</select>
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id('best'); ?>" /><?php _e('Best entries to show:', GWOLLE_GB_TEXTDOMAIN); ?></label>
+				<br />
+				<input type="text" id="<?php echo $this->get_field_id('best'); ?>" value="<?php echo $best; ?>" name="<?php echo $this->get_field_name('best'); ?>" placeholder="<?php _e('List of entry_id\'s, comma-separated', GWOLLE_GB_TEXTDOMAIN); ?>" />
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id('num_words'); ?>" /><?php _e('Number of words for each entry:', GWOLLE_GB_TEXTDOMAIN); ?></label>
+				<br />
+				<select id="<?php echo $this->get_field_id('num_words'); ?>" name="<?php echo $this->get_field_name('num_words'); ?>">
+					<?php
+					for ($i = 1; $i <= 25; $i++) {
+						echo '<option value="' . $i . '"';
+						if ( $i === $num_words ) {
+							echo ' selected="selected"';
+						}
+						echo '>' . $i . '</option>';
+					}
+					?>
+				</select>
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id('link_text'); ?>" /><?php _e('Link text:', GWOLLE_GB_TEXTDOMAIN); ?></label>
+				<br />
+				<input type="text" id="<?php echo $this->get_field_id('link_text'); ?>" value="<?php echo $link_text; ?>" name="<?php echo $this->get_field_name('link_text'); ?>" />
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id('postid'); ?>"><?php _e('Select the page of the guestbook:', GWOLLE_GB_TEXTDOMAIN); ?></label>
+				<select id="<?php echo $this->get_field_id('postid'); ?>" name="<?php echo $this->get_field_name('postid'); ?>">
+					<option value="0"><?php _e('Select page', GWOLLE_GB_TEXTDOMAIN); ?></option>
+					<?php
+					$args = array(
+						'post_type'      => 'page',
+						'nopaging'       => true,
+						'posts_per_page' => -1
+					);
+
+					$sel_query = new WP_Query( $args );
+					if ( $sel_query->have_posts() ) {
+						while ( $sel_query->have_posts() ) : $sel_query->the_post();
+							$selected = false;
+							if ( get_the_ID() == $postid ) {
+								$selected = true;
+							}
+							echo '<option value="' . get_the_ID() . '"'
+							. selected( $selected )
+							. '>'. get_the_title() . '</option>';
+						endwhile;
+					}
+					wp_reset_postdata(); ?>
+				</select>
+			</p>
+			<?php
+		}
+
+	}
+
+	function gwolle_gb_widget() {
+		register_widget('GwolleGB_Widget');
+	}
+	add_action('widgets_init', 'gwolle_gb_widget' );
+}
+
---
+++ functions/function.gwolle_gb_get_entries.php
@@ -0,0 +1,163 @@
+<?php
+
+/*
+ * gwolle_gb_get_entries
+ * Function to get guestbook entries from the database.
+ *
+ * Parameter $args is an Array:
+ * - num_entries		int: Number of requested entries
+ * - offset				int: Start after this entry
+ * - checked			string: 'checked' or 'unchecked', List the entries that are checked or not checked
+ * - deleted			string: 'deleted' or 'notdeleted', List the entries that are deleted or not deleted
+ * - spam				string: 'spam' or 'nospam', List the entries marked as spam or as no spam
+ * - email				string: The emailaddress to search for
+ *
+ * Return:
+ * - Array of objects of gwolle_gb_entry
+ * - false  if no entries found.
+ */
+
+function gwolle_gb_get_entries($args = array()) {
+	global $wpdb;
+
+	$where = " 1 = %d";
+	$values = Array(1);
+
+	if ( !is_array($args) ) {
+		return false;
+	}
+
+	if ( isset($args['checked']) ) {
+		if ( $args['checked'] == 'checked' || $args['checked'] == 'unchecked' ) {
+			$where .= "
+				AND
+				entry_isChecked = %d";
+			if ( $args['checked'] == 'checked' ) {
+				$values[] = 1;
+			} else if ( $args['checked'] == 'unchecked' ) {
+				$values[] = 0;
+			}
+		}
+	}
+	if ( isset($args['spam']) ) {
+		if ( $args['spam'] == 'spam' || $args['spam'] == 'nospam' ) {
+			$where .= "
+				AND
+				entry_isSpam = %d";
+			if ( $args['spam'] == 'spam' ) {
+				$values[] = 1;
+			} else if ( $args['spam'] == 'nospam' ) {
+				$values[] = 0;
+			}
+		}
+	}
+	if ( isset($args['deleted']) ) {
+		if ( $args['deleted'] == 'deleted' || $args['deleted'] == 'notdeleted' ) {
+			$where .= "
+				AND
+				entry_isDeleted = %d";
+			if ( $args['deleted'] == 'deleted' ) {
+				$values[] = 1;
+			} else if ( $args['deleted'] == 'notdeleted' ) {
+				$values[] = 0;
+			}
+		}
+	}
+	if ( isset($args['email']) ) {
+		$where .= "
+			AND
+			entry_author_email = %s";
+		$values[] = $args['email'];
+	}
+
+	// Limit
+	if ( is_admin() ) {
+		$perpage_option = (int) get_option('gwolle_gb-entries_per_page', 20);
+	} else {
+		$perpage_option = (int) get_option('gwolle_gb-entriesPerPage', 20);
+	}
+	$num_entries = (isset($args['num_entries']) && (int)$args['num_entries'] > 0) ? (int)$args['num_entries'] : $perpage_option;
+
+	if ( isset($args['offset']) && (int) $args['offset'] > 0 ) {
+		$limit = $args['offset'] . ", " . $num_entries;
+	} else {
+		$limit = "0, " . $num_entries;
+	}
+
+
+	$tablename = $wpdb->prefix . "gwolle_gb_entries";
+
+	$sql = "
+			SELECT
+				`entry_id`,
+				`entry_author_name`,
+				`entry_authorAdminId`,
+				`entry_author_email`,
+				`entry_author_origin`,
+				`entry_author_website`,
+				`entry_author_ip`,
+				`entry_author_host`,
+				`entry_content`,
+				`entry_date`,
+				`entry_isChecked`,
+				`entry_checkedBy`,
+				`entry_isDeleted`,
+				`entry_isSpam`
+			FROM
+				" . $tablename . "
+			WHERE
+				" . $where . "
+			ORDER BY
+				entry_date DESC
+			LIMIT
+				" . $limit . "
+			;";
+
+	$sql = $wpdb->prepare( $sql, $values );
+
+	$datalist = $wpdb->get_results( $sql, ARRAY_A );
+
+	//$wpdb->print_error();
+
+	//echo "number of rows: " . $wpdb->num_rows;
+
+	if ( count( $datalist ) == 0 ) {
+		return false;
+	} else {
+		$entries = array();
+
+		foreach ( $datalist as $data ) {
+
+			// Use the fields that the setter method expects
+			$item = array(
+				'id' => (int) $data['entry_id'],
+				'author_name' => stripslashes($data['entry_author_name']),
+				'authoradminid' => (int) $data['entry_authorAdminId'],
+				'author_email' => stripslashes($data['entry_author_email']),
+				'author_origin' => stripslashes($data['entry_author_origin']),
+				'author_website' => stripslashes($data['entry_author_website']),
+				'author_ip' => $data['entry_author_ip'],
+				'author_host' => $data['entry_author_host'],
+				'content' => stripslashes($data['entry_content']),
+				'date' => $data['entry_date'],
+				'ischecked' => (int) $data['entry_isChecked'],
+				'checkedby' => (int) $data['entry_checkedBy'],
+				'isdeleted' => (int) $data['entry_isDeleted'],
+				'isspam' => (int) $data['entry_isSpam']
+			);
+
+			$entry = new gwolle_gb_entry();
+
+			$entry->set_data( $item );
+
+			// Add entry to the array of all entries
+			$entries[] = $entry;
+		}
+		return $entries;
+	}
+	return false;
+}
+
+
+
+
