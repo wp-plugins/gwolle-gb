75c7546c818cea7f626755c706e744395a9d5a57
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-17 14:30:36 +0000

    Tag 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/tags/1.2.3@1114660 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gwolle-gb.php
+++ gwolle-gb.php
@@ -3,7 +3,7 @@
 Plugin Name: Gwolle Guestbook
 Plugin URI: http://zenoweb.nl
 Description: Gwolle Guestbook is not just another guestbook for WordPress. The goal is to provide an easy and slim way to integrate a guestbook into your WordPress powered site. Don't use your 'comment' section the wrong way - install Gwolle-GB and have a real guestbook.
-Version: 1.0.7
+Version: 1.2.3
 Author: Marcel Pol
 Author URI: http://zenoweb.nl
 License: GPLv2 or later
@@ -11,8 +11,8 @@ Text Domain: GWGB
 Domain Path: /lang/
 */
 
-/*  Copyright 2009  Wolfgang Timme  (email: gwolle@wolfgangtimme.de)
-	Copyright 2014  Marcel Pol      (email: marcel@zenoweb.nl)
+/*  Copyright 2009       Wolfgang Timme  (email: gwolle@wolfgangtimme.de)
+	Copyright 2014-2015  Marcel Pol      (email: marcel@zenoweb.nl)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -31,7 +31,15 @@ Domain Path: /lang/
 
 
 // Plugin Version
-define('GWOLLE_GB_VER', '1.0.7');
+define('GWOLLE_GB_VER', '1.2.3');
+
+
+/*
+ * FIXME: add things to title and description for SEO
+ * https://wordpress.org/support/topic/seo-optimization-4?replies=2#post-6532654
+ * Title meta that looks like: title tag - page number
+ * Description meta, one message of the page would be perfect.
+ */
 
 
 /*
@@ -52,7 +60,7 @@ $wpdb->gwolle_gb_log = $wpdb->prefix . 'gwolle_gb_log';
 
 
 // Classes
-include_once( GWOLLE_GB_DIR . '/functions/class.gwolle_gb_entry.php' );
+include_once( GWOLLE_GB_DIR . '/functions/class.entry.php' );
 
 // Functions for the frontend
 include_once( GWOLLE_GB_DIR . '/frontend/index.php' );
@@ -65,25 +73,26 @@ include_once( GWOLLE_GB_DIR . '/admin/ajax.php' );
 include_once( GWOLLE_GB_DIR . '/admin/installSplash.php' );
 include_once( GWOLLE_GB_DIR . '/admin/page-editor.php' );
 include_once( GWOLLE_GB_DIR . '/admin/page-entries.php' );
+include_once( GWOLLE_GB_DIR . '/admin/page-export.php' );
 include_once( GWOLLE_GB_DIR . '/admin/page-gwolle-gb.php' );
 include_once( GWOLLE_GB_DIR . '/admin/page-import.php' );
 include_once( GWOLLE_GB_DIR . '/admin/page-settings.php' );
 include_once( GWOLLE_GB_DIR . '/admin/upgrade.php' );
 
 // General Functions
-include_once( GWOLLE_GB_DIR . '/functions/function.gwolle_gb_akismet.php' );
-include_once( GWOLLE_GB_DIR . '/functions/function.gwolle_gb_get_entries.php' );
-include_once( GWOLLE_GB_DIR . '/functions/function.gwolle_gb_get_entry_count.php' );
-include_once( GWOLLE_GB_DIR . '/functions/function.gwolle_gb_log.php' );
-include_once( GWOLLE_GB_DIR . '/functions/function.gwolle_gb_misc.php' );
+include_once( GWOLLE_GB_DIR . '/functions/function.akismet.php' );
+include_once( GWOLLE_GB_DIR . '/functions/function.get_entries.php' );
+include_once( GWOLLE_GB_DIR . '/functions/function.get_entry_count.php' );
+include_once( GWOLLE_GB_DIR . '/functions/function.log.php' );
+include_once( GWOLLE_GB_DIR . '/functions/function.misc.php' );
 
 // Actions
 include_once( GWOLLE_GB_DIR . '/actions.php' );
 
-// Widget
+// Frontend Widget
 include_once( GWOLLE_GB_DIR . '/frontend/widget.php' );
 
-// Dashboard widget (for the WP-Admin dashboard)
+// Dashboard Widget (for the WP-Admin dashboard)
 include_once( GWOLLE_GB_DIR . '/admin/dashboard-widget.php' );
 
 
@@ -101,7 +110,5 @@ function gwolle_gb_activation() {
 	}
 }
 register_activation_hook(__FILE__, 'gwolle_gb_activation');
-
-?>
 
 
