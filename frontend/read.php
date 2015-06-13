6e97c928d8c6fa6f5a94421e784e6411ba74483e
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-13 13:02:22 +0000

    Change 'at' time to 'on'
    git-svn-id: https://plugins.svn.wordpress.org/gwolle-gb/trunk@1180183 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- frontend/read.php
+++ frontend/read.php
@@ -198,11 +198,11 @@ function gwolle_gb_frontend_read() {
 				$entry_output .= '<span class="gb-datetime">
 							<span class="gb-date"> ';
 				if ( isset($read_setting['read_name']) && $read_setting['read_name']  === 'true' ) {
-					$entry_output .= __('wrote at', GWOLLE_GB_TEXTDOMAIN) . ' ';
+					$entry_output .= __('wrote on', GWOLLE_GB_TEXTDOMAIN) . ' ';
 				}
 				$entry_output .=  date_i18n( get_option('date_format'), $entry->get_date() ) . '</span>';
 				if ( isset($read_setting['read_datetime']) && $read_setting['read_datetime']  === 'true' ) {
-					$entry_output .= '<span class="gb-time"> ' . __('at', GWOLLE_GB_TEXTDOMAIN) . ' ' . trim(date_i18n( get_option('time_format'), $entry->get_date() )) . '</span>';
+					$entry_output .= '<span class="gb-time"> ' . __('on', GWOLLE_GB_TEXTDOMAIN) . ' ' . trim(date_i18n( get_option('time_format'), $entry->get_date() )) . '</span>';
 				}
 				$entry_output .= ':</span> ';
 			}
